b3e7bb53f265442638b0e74614bb265f8cd87259
Leo-Wolff <leokramer17@gmail.com>
2023-12-20 22:56:19 +0100

    Add file structure + type task function

---
+++ script/script.js
@@ -0,0 +1,43 @@
+// THIS CODE IS COMPLETELY CHAT.GPT IT WILL BE EDITED TO BE MORE EFFICIENT AND FLEXIBLE
+
+// Get the textarea element
+var textarea = document.getElementById("myTextarea")
+
+// Define the text you want to print
+var premadeText = "Your premade text here."
+
+// Define a variable to keep track of the current index
+var currentIndex = 0
+
+// Add an event listener for any key press
+document.addEventListener("keydown", function (event) {
+	// Check if the pressed key is an alphabetical letter
+	if (/^[a-zA-Z]$/.test(event.key)) {
+		// Check if there are more letters to print
+		if (currentIndex < premadeText.length) {
+			// Append the next letter to the textarea content
+			textarea.value += premadeText.charAt(currentIndex)
+			// Increment the current index for the next key press
+			currentIndex++
+		}
+	} else if (event.key === "Backspace") {
+		// Check if there is text to delete
+		if (textarea.value.length > 0) {
+			// Delete the last character from the textarea content
+			textarea.value = textarea.value.slice(0, -1)
+			// Decrement the current index to stay in sync with the textarea content
+			currentIndex--
+		}
+		// Prevent the default behavior for Backspace and Delete
+		event.preventDefault()
+	} else {
+		// Prevent other keys from triggering default behavior
+		event.preventDefault()
+	}
+})
+
+// Clear the textarea content and reset the index when the page is loaded
+window.addEventListener("load", function () {
+	textarea.value = ""
+	currentIndex = 0
+})
