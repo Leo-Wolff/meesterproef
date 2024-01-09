1f5bf1a2b3ca4fa45d5b6fb8e51771306447037d
Leo-Wolff <leokramer17@gmail.com>
2024-01-09 10:26:43 +0100

    Fix text task + organise script.js

--- script/script.js
+++ script/script.js
@@ -36,38 +36,69 @@ let currentCharacter = 0
 console.log(textArea)
 console.log(taskText)
 
-document.addEventListener("keydown", (event) => {
-	// Check if the lvl1Text is actually being used
-	if (document.activeElement == taskText) {
-		if (/^[a-zA-Z]$/.test(event.key)) {
-			// If currentIndex is smaller than premadeText.length than there will be more text to type
-			if (currentCharacter < taskText.length) {
-				// Adds the next character to the existing text
-				textArea.value += taskText.charAt(currentCharacter)
+textArea.addEventListener("keydown", (event) => {
+	if (/^[a-zA-Z]$/.test(event.key)) {
+		// If currentCharacter is smaller than taskText.length than there will be more text to type
+		if (currentCharacter < taskText.length) {
+			// Adds the next character to the existing text
+			textArea.value += taskText.charAt(currentCharacter)
 
-				// Increase currentIndex so that the next iteration of the function can type the next character
-				currentCharacter++
-			}
-		} else {
-			// Prevent other keys from triggering default behavior
-			event.preventDefault()
+			// Increase currentCharacter so that the next iteration of the function can type the next character
+			currentCharacter++
 		}
+	} else {
+		// Prevent other keys from triggering default behavior
+		event.preventDefault()
 	}
 })
 
+// Reset function for the whole level
 const resetText = () => {
 	textArea.value = ""
 	currentCharacter = 0
 }
 
-// Clear the textarea content and reset the currentCharacter when the page is loaded
+// Reset the level when the page is loaded
 window.addEventListener("load", () => {
 	resetText()
 })
 
+//
+// FANTASY - LEVELS
+//
+
+// VARIABLES
+const fantasyLevels = document.querySelectorAll("article:first-of-type section")
+// console.log(levels)
+let currentFantasyLevel = 0
+const stars = fantasyLevels[currentFantasyLevel].querySelectorAll("section div")
+// console.log(lvl1Stars)
+
+stars.forEach((a) => {
+	a.addEventListener("click", (event) => {
+		// Add the "active" class to the clicked star
+		event.target.classList.add("active")
+
+		// Check if all stars have the "active" class
+		if (Array.from(stars).every((b) => b.classList.contains("active"))) {
+			fantasyLevels[currentFantasyLevel]
+				.querySelector("section:first-of-type")
+				.classList.add("hidden")
+			fantasyLevels[currentFantasyLevel]
+				.querySelector("section:last-of-type")
+				.classList.remove("hidden")
+
+			currentFantasyLevel++
+		}
+	})
+})
+
+//
 // SCROLL TRANSITION
+//
+
+// VARIABLES
 let scrollArticles = false
-const articles = document.querySelectorAll("article")
 
 const scrollTransition = () => {
 	if (scrollArticles == false) {
@@ -94,22 +125,3 @@ const scrollTransition = () => {
 }
 
 // scrollTransition()
-
-// STAR SELECT
-const levels = document.querySelectorAll("article:first-of-type section")
-// console.log(levels)
-const lvl1Stars = levels[0].querySelectorAll("section div")
-// console.log(lvl1Stars)
-
-lvl1Stars.forEach((star) => {
-	star.addEventListener("click", (event) => {
-		// Add the "active" class to the clicked star
-		event.target.classList.add("active")
-
-		// Check if all stars have the "active" class
-		if (Array.from(lvl1Stars).every((a) => a.classList.contains("active"))) {
-			levels[0].querySelector("section:first-of-type").classList.add("hidden")
-			levels[0].querySelector("section:last-of-type").classList.remove("hidden")
-		}
-	})
-})
