5c57d30114ac95e920866a47a996ce88dc054320
Leo-Wolff <leokramer17@gmail.com>
2024-01-15 14:45:06 +0100

    Update visuals

--- script/database.js
+++ script/database.js
@@ -1,35 +1,44 @@
 const realityData = {
 	0: {
+		taskDescription:
+			"Write an email to teacher for clarification on the assignment.",
 		taskText:
 			"Dear Teacher, \n \n I would like to know more about the current assignment since the deadline is coming up and the rubric is unclear. If you have the time I would appreciate it if you could answer when is the assignment due exactly. I could only find what week it is due but not the exact date and time. \n \n Thank you in advance and I eagerly await your response. \n \n Best regards, \n Adrien Larsen",
 	},
 	1: {
+		taskDescription: "Reply to teacher’s email.",
 		taskText:
 			"Hi Teacher, \n \n I did indeed manage to sign up for the class. I would still like to schedule a meeting with you if possible. Next week I will be available in the afternoons of Monday-Wednesday. Otherwise I can also work all day on Friday. \n \n Let me know what works best for you. \n \n Thanks for your help, \n Adrien",
 	},
 	2: {
+		taskDescription: "Read and summarise chapter 4.",
 		taskText:
 			"Scout was giving me a look. \n “What?” I asked. \n “Are you using incantations or something?” Oh shit. They must have spotted me daydreaming. I quickly came up with a lie so I wouldn’t get anymore questions, \n “Oh uhm, that’s normal in Canada, helps against the cold.” \n “How strange.” Commented Helias before signifying where we should turn next. “We’re almost there.”",
 	},
 	3: {
+		taskDescription: "Prepare questions for meeting.",
 		taskText:
 			"- How big is the class and how big is the chance that I can get in? \n - Who has priority for sign up? \n - How many study hours? \n - What is the schedule/curriculum? \n - Are there any additional preparations or programs that I need to arrange? \n - When is the sign up deadline?",
 	},
 	4: {
+		taskDescription: "Message tutor about elective.",
 		taskText:
 			"Hi Tutor. This week I started looking for an elective for my second semester. I was thinking about taking one in Storytelling. I am currently now in the process of requesting permission for this from the examination board. I just want to give an update on this, so that's why I'm writing this message",
 	},
 	5: {
+		taskDescription: "Plan what to do this week.",
 		taskText:
 			"Have to do \n - Keyboard navigation \n - Download necessary videos \n - Create all levels \n - Mystery spot easter egg \n - Source list \n - Process + apply zero state feedback \n \n Want to do \n - Easter egg \n - Refactor HTML, CSS, JS \n - W3C check \n - Add sound effects \n - Level progression system \n ",
 	},
 	6: {
+		taskDescription: "Message best friend to hang out.",
 		taskText:
 			"Hey bud! I know with the pandemic going on that we can’t really go outside and hang out, but honestly I’m lacking social interactions. I can’t meet up irl, my dad still has corona so I gotta stay quarantined. Maybe we can game together on Discord tomorrow? Or any other day really. Lmk what you think :)",
 	},
 	7: {
+		taskDescription: "Update assignment log.",
 		taskText:
-			"For my retake, I wanted to make the assignment easier for myself. From the start of the course, I noticed that I was struggling with my chosen subject all the time. \n \n To avoid having to start all over again, I will still focus on books, but with a different direction. With this train of thought I noticed Rick Riordan: my favorite author.",
+			"For my resit, I wanted to make the assignment easier for myself. From the start of the course, I noticed that I was struggling with my chosen subject all the time. \n \n To avoid having to start all over again, I will still focus on books, but with a different direction. With this train of thought I noticed Rick Riordan: my favorite author.",
 	},
 }
 
